20e723437e051b56ea19ced28a80fc8e85f2c0b4
Plasma09 <malinurcameron@gmail.com>
2024-09-28 12:06:02 +0200

    2: changed get_character_count to list all character in a new line

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 book_path = "books/frankenstein.txt"
-char_list = list("qwertyuiopasdfghjklzxcvbnm")
+valid_char = list("qwertyuiopasdfghjklzxcvbnm")
+
 
 def main():
     book = get_book_text(book_path)
     word_count = get_word_count(book)
     character_count = get_character_count(book)
-    print(f"Word count : {word_count}")
+    print(f"--- Beginning the analysis of {book_path} ---")
+    print("================================")
+    print(f"{word_count} words were found in the document")
+    print("================================")
     print(character_count)
 
-def get_word_count(book):
-    words = book.split()
-    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
+def get_word_count(book):
+    words = book.split()
+    return len(words)
+
+
 def get_character_count(book):
     char_dict = {}
+    char_list = []
     lower_string = book.lower()
     for char in lower_string:
-        if char in char_list:
+        if char in valid_char:
             if char in char_dict:
                 char_dict[char] += 1
             else:
                 char_dict[char] = 1
-    return char_dict
 
+    result_list = []
+    for char in char_dict:
+        count = char_dict[char]
+        result_list.append(f"The letter '{char}' was found {count} times")
+    
+
+    return "\n".join(result_list)
 
 main()
